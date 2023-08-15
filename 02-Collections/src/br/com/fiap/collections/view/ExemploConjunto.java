1b444df41a2d66adfe6c451ff3a68ac587758986
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-08-15 19:01:18 -0300

    Coleções

--- 02-Collections/src/br/com/fiap/collections/view/ExemploConjunto.java
+++ 02-Collections/src/br/com/fiap/collections/view/ExemploConjunto.java
@@ -16,6 +16,7 @@ public class ExemploConjunto {
 		conjunto.add("Vermelho");
 		
 		//Adicionar um valor repetido
+
 		conjunto.add("Roxo"); //foi ignorado
 		
 		//Exibir a quantidade de elementos no conjunto
