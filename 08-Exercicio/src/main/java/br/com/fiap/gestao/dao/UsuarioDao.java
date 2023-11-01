9b766c8d7a88e333f6e17eeb6eccdc4fba9e722a
Thiago Yamamoto <thiagoyama@gmail.com>
2023-11-01 10:06:00 -0300

    Ajustes

---
+++ 08-Exercicio/src/main/java/br/com/fiap/gestao/dao/UsuarioDao.java
@@ -0,0 +1,11 @@
+package br.com.fiap.gestao.dao;
+
+import br.com.fiap.gestao.model.Usuario;
+
+public class UsuarioDao {
+
+	public void cadastrar(Usuario usuario) {
+		
+	}
+	
+}
---
+++ 08-Exercicio/src/main/java/br/com/fiap/gestao/model/Usuario.java
@@ -0,0 +1,68 @@
+package br.com.fiap.gestao.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Usuario {
+
+	private int id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String cpf;
+	
+	private LocalDateTime dataCadastro;
+	
+	private LocalDate dataNascimento;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
